94330c9b71389a98483d92e7c5d9cc4d767af280
kidkai25 <40673009+kidkai25@users.noreply.github.com>
2020-04-05 01:48:54 +0530

    Added Command Model to API Project

---
+++ src/CommandAPI/Models/Command.cs
@@ -0,0 +1,7 @@
+public class Command
+{
+    public int Id { get; set; }
+    public string HowTo { get; set; }
+    public string Platform { get; set; }
+    public string CommandLine { get; set; }
+}
